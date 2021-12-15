629c49aa538dabe9a8b4c9e27bf84624578d2a29
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-12-16 00:35:16 +0530

    initial commit

---
+++ app/src/main/java/com/example/eventmangementapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eventmangementapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_screen_activity)
+    }
+}
